5a09d053b97fc39e0d0e1a678aff686de7d77169
naijelo <naijel.a@gmail.com>
2022-09-03 23:14:00 +0300

    fix: links to categories from home page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,15 @@
+import {useNavigate} from "react-router-dom";
+
 import "./directory-item.styles.scss";
 
 const DirectoryItem = ({ category }) => {
-  const {imageUrl, title} = category;
+  const {imageUrl, title, route} = category;
+  const navigate = useNavigate();
+
+  const onNavigateFunction = () => navigate(route);
+
   return (
-    <div className="directory-item-container" >
+    <div className="directory-item-container" onClick={onNavigateFunction}>
       <div className="background-image" style={{
         backgroundImage: `url(${imageUrl})`
       }} />
