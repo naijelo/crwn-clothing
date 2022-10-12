a6d83d7394ac4346a9df73de9895f4aed7c0acc3
naijelo <naijel.a@gmail.com>
2022-10-12 13:29:02 +0300

    feat: use redux-thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,29 @@
 import { compose, createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if(!action.type) {
-    return next(action);
-  }
-  next(action);
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist : ["cart"],
 }
 
-const middleWares = [loggerMiddleware]
+const middleWares = [process.env.NODE_ENV === "development" && logger, thunk,].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+(process.env.NODE_ENV !== 'production' &&
+window &&
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+compose;
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
