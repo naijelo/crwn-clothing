8fb10f3b14937eed665db6fb6758fb24c9a96251
naijelo <naijel.a@gmail.com>
2023-01-06 23:30:05 +0300

    feat: add styled components and typescript for inputs and sign forms

--- src/components/button/button.styles.tsx
+++ src/components/button/button.styles.tsx
@@ -7,9 +7,9 @@ export const BaseButton = styled.button`
   width: auto;
   height: 50px;
   letter-spacing: 0.5px;
-  line-height: 50px;
+  line-height: 16px;
   padding: 0 35px 0 35px;
-  font-size: 15px;
+  font-size: 14px;
   background-color: black;
   color: white;
   text-transform: uppercase;
